c9441285e3503d1fdf6c3cb162c0ff50a89f5f52
Ahmed-osama <ahmed.osama@tajawal.com>
2018-10-22 12:45:51 +0200

    console errors

--- burger-builder/src/componenets/layout/Layout.jsx
+++ burger-builder/src/componenets/layout/Layout.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import ToolBar from '../navigation/toolbar/toolbar';
 import SideDrawer from '../navigation/sideDrawer/sideDrawer';
 
-const layout = props => (
-    <Aux>
-        <ToolBar />
-        <SideDrawer/>
-        <main className={classes.content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class layout extends Component  {
+    state = {
+        showSideDrawer : true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer : false
+        })
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState(prevstate => {
+           return {showSideDrawer : !prevstate.showSideDrawer} 
+        })
+    }
+    render(){
+        return (
+        <Aux>
+            <ToolBar  drawerToggleClicked = {this.sideDrawerToggleHandler} />
+            <SideDrawer 
+                closed={this.sideDrawerClosedHandler}
+                open={this.state.showSideDrawer}
+            />
+            <main className={classes.content}>
+                {this.props.children}
+            </main>
+        </Aux>)
+    }
+}
+   
 export default layout
--- burger-builder/src/componenets/navigation/sideDrawer/sideDrawer.jsx
+++ burger-builder/src/componenets/navigation/sideDrawer/sideDrawer.jsx
@@ -2,13 +2,18 @@ import React  from 'react';
 import classes from  './sideDrawer.css';
 import NavigationItems from '../navigationItems/navigationItems';
 import Logo from '../../logo/logo';
+import BackDrop from '../../ui/backdrop/backDrop';
+import Aux from '../../../hoc/Aux';
 
 const SideDrawer = props => (
-  <div className={classes.SideDrawer}>
-    <Logo height={'11%'}/>
-    <nav>
-      <NavigationItems/>
-    </nav>
-  </div>
+  <Aux>
+    <BackDrop clicked={props.close}/>
+    <div className={classes.SideDrawer}>
+      <Logo height={'11%'}/>
+      <nav>
+        <NavigationItems/>
+      </nav>
+    </div>
+  </Aux>
 );
 export default SideDrawer;
